c9403960c5a8de1e16894476bc4fe89b9f737e11
Aura Alba <auramile1993@gmail.com>
2026-09-22 19:23:34 -0500

    feat: agregar funciones suma y division

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,10 @@
-from calculator import multiplicacion, resta
+import pytest
+from calculator import division, multiplicacion, resta, suma
+
+
+def test_suma():
+    assert suma(4, 6) == 10
+    assert suma(-1, 1) == 0
 
 
 def test_resta():
@@ -7,3 +13,13 @@ def test_resta():
 
 def test_multiplicacion():
     assert multiplicacion(4, 5) == 20
+
+
+def test_division_correcta():
+    assert division(10, 2) == 5
+    assert division(9, 3) == 3
+
+
+def test_division_por_cero():
+    with pytest.raises(ValueError, match="No se puede dividir entre cero."):
+        division(10, 0)
